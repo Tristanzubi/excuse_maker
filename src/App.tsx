f8fada21abee9df06ce232019e355a62a51adbac
B-Maxp <max.bpr@outlook.fr>
2025-04-07 16:06:54 +0200

    cread header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,45 @@
 import './App.css'
+import Header from './components/Header/Header';
+
+
+const button = [
+  {
+    emoji: "👮🏻‍♂️",
+    answer1:"nlkdsnlcnl",
+    answer2:"nlkdsnlcnl" ,
+    answer3:"nlkdsnlcnl" ,
+    answer4:"nlkdsnlcnl" 
+   },
+  {
+    emoji: "🐋",
+    answer1:"nlkdsnlcnl",
+    answer2:"nlkdsnlcnl" ,
+    answer3:"nlkdsnlcnl" ,
+    answer4:"nlkdsnlcnl" 
+   },
+  {
+    emoji: "🤮",
+    answer1:"nlkdsnlcnl",
+    answer2:"nlkdsnlcnl" ,
+    answer3:"nlkdsnlcnl" ,
+    answer4:"nlkdsnlcnl" 
+   },
+  
+];
+
 
 function App() {
   return (
     <>
+      <Header/>
+      <main>
+        <h2>L'art de l'excuse, à portée de clic !</h2>
+        <section>
+          {/* <Card /> */}
+        </section>
+        <p></p>
+      </main>
+      {/* <Footer /> */}
     </>
   )
 }
